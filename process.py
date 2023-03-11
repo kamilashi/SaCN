249119218d72374065f7d264d1b2f80c9b72bd0f
Kamila <64157493+kamilashi@users.noreply.github.com>
2023-03-11 14:01:58 +0100

    locale fixes untested

--- process.py
+++ process.py
@@ -2,15 +2,18 @@ import sys
 import os
 import json
 import fDiscord
-import data.locale as locale
+import data.locales as locales
 
 save_path = "./data/save.json";
+locale_path = "./data/locale.json";
+
 enum_attributes = [];
 enum_passwords = [];
 enum_entry_text   = [];
 enum_riddles = [];
 enum_key_pieces = [];
 stages = [];
+locale = "";
 
 class Stage:
     #static vars defined here
@@ -32,8 +35,11 @@ class Actor:
         self.attributes = attributes_dict;
 
 def init():
-    set_locale("eng");
-    initActor();
+    global locale
+    locale = loadLocale();
+    set_locale(locale);
+    loadActor();
+    actor_locale(locale) #add check!
 
 def main(msg):
     print("debug info here\n");
@@ -49,7 +55,7 @@ def main(msg):
                            fDiscord.bold(stages[i].entry_text) + "\n" + \
                            stages[i].reward_attribute + " +1 for your character.\n\n" +\
                            "You get a key piece:" + \
-                           "\n" + stages[i].reward_key_piece[i] + "\n" +\
+                           "\n" + stages[i].reward_key_piece + "\n" +\
                            "keep it safe!\n\n" + \
                            stages[i].reward_riddle;
 
@@ -69,16 +75,22 @@ def saveActor(obj):
     json_object = json.dumps(obj.__dict__, indent=4)
     with open(save_path, "w+") as outfile:
         outfile.write(json_object)
+    outfile.close();
     return;
 
 def loadActor():
-    with open(save_path, 'r') as openfile:
-        actor_dict = json.load(openfile);
-    actor = Actor(actor_dict['next_stage'], actor_dict['attributes']);
-    openfile.close()
+    if (os.path.exists(save_path)): #load if exists
+        with open(save_path, 'r') as openfile:
+            actor_dict = json.load(openfile);
+        actor = Actor(actor_dict['next_stage'], actor_dict['attributes']);
+        openfile.close();
+    else: #create new if not
+        actor = Actor(0, dict.fromkeys(enum_attributes, 0));
+        saveActor(actor);
+        return;
     return actor;
 
-def initActor():
+def initActor():    #only dev function, use carefully
     if(os.path.exists(save_path)):
         return;
     else:
@@ -109,35 +121,59 @@ def gameToString():
     string += "\n" + "--" * 30+ "\n" ;
     string += fDiscord.bold("enum_key_pieces: ");
     string += "\n" + str(enum_key_pieces);
+    # string += "\n\n";
+    # string += fDiscord.bold("stages: ");
+    # string += "\n" + str(stages);
+
     return string;
 
-def resetActor():
+def resetActor(): #only dev function, use carefully
     os.remove(save_path);
+
+def loadLocale():
+    if (os.path.exists(locale_path)): #load if exists
+        with open(locale_path, 'r') as openfile:
+            lang = json.load(openfile);
+        openfile.close();
+    else: #create new if not
+        lang = "eng"; # default english
+        json_object = json.dumps(lang);
+        with open(locale_path, "w+") as outfile:
+            outfile.write(json_object)
+        outfile.close();
+    return lang;
 
 
 # game language switch, default is english
-def set_locale(lang = "eng"):
+def set_locale(lang): # to-do: refactor to reload
+    global locale
     global enum_attributes
     global enum_passwords
     global enum_entry_text
     global enum_riddles
     global enum_key_pieces
     if(lang == "eng"):
-        enum_attributes = locale.eng.enum_attributes;
-        enum_passwords = locale.eng.enum_passwords;
-        enum_entry_text = locale.eng.enum_entry_text;
-        enum_riddles = locale.eng.enum_riddles;
-        enum_key_pieces = locale.eng.enum_key_pieces;
+        enum_attributes = locales.eng.enum_attributes;
+        enum_passwords = locales.eng.enum_passwords;
+        enum_entry_text = locales.eng.enum_entry_text;
+        enum_riddles = locales.eng.enum_riddles;
+        enum_key_pieces = locales.eng.enum_key_pieces;
     elif(lang == "ru"):
-        enum_attributes = locale.ru.enum_attributes;
-        enum_passwords = locale.ru.enum_passwords;
-        enum_entry_text = locale.ru.enum_entry_text;
-        enum_riddles = locale.ru.enum_riddles;
-        enum_key_pieces = locale.ru.enum_key_pieces;
+        enum_attributes = locales.ru.enum_attributes;
+        enum_passwords = locales.ru.enum_passwords;
+        enum_entry_text = locales.ru.enum_entry_text;
+        enum_riddles = locales.ru.enum_riddles;
+        enum_key_pieces = locales.ru.enum_key_pieces;
+    global stages
     stages = [];  # reset just in case
     for i in range(0, len(enum_attributes)):
         stage = Stage(i);
         stages.append(stage);
+    locale = lang;  # save
+    json_object = json.dumps(locale);
+    with open(locale_path, "w+") as outfile:
+        outfile.write(json_object)
+
 
 # resave actor to new language
 def actor_locale(lang):
